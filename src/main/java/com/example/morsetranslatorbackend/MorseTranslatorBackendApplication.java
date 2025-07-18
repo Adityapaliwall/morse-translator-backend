c2838fac172493aa11170c4f2fb307e26519fd88
Aditya <114735447+Adityapaliwall@users.noreply.github.com>
2025-07-18 21:41:46 +0530

    MorseTranslatorBackendApplication.java

---
+++ src/main/java/com/example/morsetranslatorbackend/MorseTranslatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.morsetranslatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorseTranslatorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorseTranslatorBackendApplication.class, args);
+    }
+
+}
